378d85eeecc521faf876826770fd849d1a8acf84
rjabdurrahman <md.rahman0296@gmail.com>
2019-10-03 18:01:28 +0600

    Changed: Debtors to rest api

--- pages/receivable.js
+++ pages/receivable.js
@@ -1,11 +1,15 @@
 app.controller('ReceivableCntlr', function ($scope, $firebaseArray) {
     $scope.title = "Debtor";
-    var ref = firebase.database().ref().child('receivables');
-    dbReceivables = $scope.comReceivables = $firebaseArray(ref);
-    $scope.comReceivables.$loaded().then(function () {
-        $scope.load = true;
-        if ($scope.comReceivables.length == 0) {
-            $scope.nodata = true;
-        }
-    });
+    axios.get(apiUrl + 'chart/debtors')
+        .then(function (res) {
+            dbReceivables = $scope.comReceivables = res.data;
+            $scope.$applyAsync();
+            $scope.load = true;
+            if ($scope.comReceivables.length == 0) {
+                $scope.nodata = true;
+            }
+        })
+        .catch(function (error) {
+            notify(error.message, 2);
+        });
 });

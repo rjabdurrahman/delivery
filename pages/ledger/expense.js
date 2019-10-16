f9f6f97b347f21f3fa35a1508d5cf38f884a5892
rjabdurrahman <md.rahman0296@gmail.com>
2019-10-17 01:15:18 +0600

    Updated: Expense ledger api and ui

--- pages/ledger/expense.js
+++ pages/ledger/expense.js
@@ -13,29 +13,20 @@ app.controller('ExLedgerCntlr', function ($scope, $firebaseArray) {
     $scope.reCalculate = function (e) {
         coCen = e.target.textContent;
         dropFunction();
-        $print(e.target.parentElement);
         $scope.records = [];
-        fsDb.collection("JournalForm").where('ACCodes', 'array-contains', code.value).where("date", ">=", dateToNum(dateFrom.value)).where("date", "<=", dateToNum(dateTo.value)).where("costCenter", "==", coCen).get()
-            .then(function (snapshot) {
-                $scope.recShow = true;
-                if (snapshot.size == 0) {
-                    $scope.nodata = true;
-                    $scope.$applyAsync();
-                }
-                else {
-                    snapshot.docs.forEach(element => {
-                        let obj = element.data();
-                        obj.sCode = code.value;
-                        $scope.records.push(obj);
-                        $scope.nodata = false;
-                        $scope.$applyAsync();
-                    });
-                }
+        axios.post(apiUrl + 'ledger/expense', {operation: 2, costCenter: coCen, ACCode : code.value, dateFrom : dateToNum(dateFrom.value), dateTo : dateToNum(dateTo.value)})
+            .then(function (res) {
+                $scope.records.push(...res.data);
+                $scope.nodata = false;
+                $scope.$applyAsync();
             })
             .catch(function (err) {
-                $print(err);
+                alert(err);
+                e.target.disabled = false;
+                e.target.textContent = 'Calculate';
             });
     }
+
     $scope.debitTaker = function (e) {
         name = e.target.parentElement.previousElementSibling.lastElementChild;
         code = name.parentElement.previousElementSibling.lastElementChild.previousElementSibling;
@@ -88,56 +79,28 @@ app.controller('ExLedgerCntlr', function ($scope, $firebaseArray) {
         axios.post(apiUrl + 'ledger/expense', {operation: 1, ACCode : code.value, dateFrom : dateToNum(dateFrom.value), dateTo : dateToNum(dateTo.value)})
             .then(function (res) {
                 $scope.records.push(...res.data);
+                $scope.costCenters = [...new Set(res.data.map( x => x.costCenter))];
                 $scope.nodata = false;
                 $scope.$applyAsync();
-                $print('Pre Records');
-                $print($scope.records);
+                e.target.disabled = false;
+                e.target.textContent = 'Calculate';
             })
             .catch(function (err) {
                 $print(err);
                 e.target.disabled = false;
                 e.target.textContent = 'Calculate';
             });
-        // fsDb.collection("JournalForm").where('ACCodes', 'array-contains', code.value).where("date", ">=", dateToNum(dateFrom.value)).where("date", "<=", dateToNum(dateTo.value)).get()
-        //     .then(function (snapshot) {
-        //         $scope.recShow = true;
-        //         if (snapshot.size == 0) {
-        //             e.target.disabled = false;
-        //             e.target.textContent = 'Calculate';
-        //             $scope.nodata = true;
-        //             $scope.$applyAsync();
-        //         }
-        //         else {
-        //             snapshot.docs.forEach(element => {
-        //                 let obj = element.data();
-        //                 obj.sCode = code.value;
-        //                 $print(obj);
-        //                 if (obj.costCenter && $scope.costCenters.indexOf(obj.costCenter) < 0) $scope.costCenters.push(obj.costCenter);
-        //                 $scope.records.push(obj);
-        //                 $scope.nodata = false;
-        //                 $scope.$applyAsync();
-        //                 $print($scope.records);
-        //                 e.target.disabled = false;
-        //                 e.target.textContent = 'Calculate';
-        //             });
-        //         }
-        //     })
-        //     .catch(function (err) {
-        //         $print(err);
-        //         e.target.disabled = false;
-        //         e.target.textContent = 'Calculate';
-        //     });
     }
 
     $scope.arrTotal = function (arr, index, t) {
         if (index == -1) return 0;
         let total = 0;
         for (i = 0; i <= index; i++) {
-            if (arr[i].sCode == arr[i].ACCodes[0] && (t == 0 || t == 1)) {
-                total += arr[i].debitCredit[0].drAmount;
+            if (arr[i].type == 'Dr' && (t == 0 || t == 1)) {
+                total += arr[i].drAmount;
             }
-            if (arr[i].sCode == arr[i].ACCodes[1] && (t == 0 || t == 2)) {
-                total -= arr[i].debitCredit[1].crAmount;
+            if (arr[i].type == 'Cr' && (t == 0 || t == 2)) {
+                total -= arr[i].crAmount;
             }
         }
         return total;
